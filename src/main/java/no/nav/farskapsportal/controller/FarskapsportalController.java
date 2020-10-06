1550377bfd3e4d660150a201f29b6279323826a3
s148719 <jan.kjetil.skar@nav.no>
2020-10-06 15:59:37 +0200

    Legge til cross origin på kontroller

--- src/main/java/no/nav/farskapsportal/controller/FarskapsportalController.java
+++ src/main/java/no/nav/farskapsportal/controller/FarskapsportalController.java
@@ -17,6 +17,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/farskapsportal")
 @ProtectedWithClaims(issuer = ISSUER)
+@CrossOrigin(origins = "https://farskapsportal.*.no")
 @Slf4j
 public class FarskapsportalController {
 
