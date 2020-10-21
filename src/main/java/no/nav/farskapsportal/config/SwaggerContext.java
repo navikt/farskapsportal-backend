1f2be61743d58a82376010ebdb7384687dfcd8f3
s148719 <jan.kjetil.skar@nav.no>
2020-10-21 15:39:44 +0200

    Swagger-ui rollback to 2.9.2

--- src/main/java/no/nav/farskapsportal/config/SwaggerContext.java
+++ src/main/java/no/nav/farskapsportal/config/SwaggerContext.java
@@ -17,10 +17,10 @@ import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerContext {
 
   @Bean
