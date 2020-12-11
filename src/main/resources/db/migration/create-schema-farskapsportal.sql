fced09e76bb8c7a2edd998d6ac9f4fb50f00866a
s148719 <jan.kjetil.skar@nav.no>
2020-12-11 14:49:49 +0100

    Flyway - postgresql - hikariCP

---
+++ src/main/resources/db/migration/create-schema-farskapsportal.sql
@@ -0,0 +1,12 @@
+-- SCHEMA: farskapsportal
+
+-- DROP SCHEMA farskapsportal ;
+
+CREATE SCHEMA farskapsportal
+    AUTHORIZATION farskapsportal_api;
+
+COMMENT ON SCHEMA farskapsportal
+    IS 'standard farskapsportal schema';
+
+GRANT ALL ON SCHEMA farskapsportal TO farskapsportal_api;
+
