72246bbb7a9d9289c19a722c7d93478463957ef2
s148719 <jan.kjetil.skar@nav.no>
2021-02-05 15:15:20 +0100

    Update DifiEsigneringConfig.java

--- src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
+++ src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
@@ -1,11 +1,12 @@
 package no.nav.farskapsportal.config;
 
 import static no.nav.farskapsportal.FarskapsportalApplication.PROFILE_LIVE;
-import static no.nav.farskapsportal.FarskapsportalApplication.PROFILE_LOCAL;
 
+import com.google.cloud.spring.secretmanager.SecretManagerTemplate;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import lombok.extern.slf4j.Slf4j;
 import no.digipost.signature.client.Certificates;
 import no.digipost.signature.client.ClientConfiguration;
 import no.digipost.signature.client.ServiceUri;
@@ -13,22 +14,32 @@ import no.digipost.signature.client.direct.DirectClient;
 import no.digipost.signature.client.security.KeyStoreConfig;
 import no.nav.farskapsportal.consumer.esignering.DifiESignaturConsumer;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-// TODO: Fikse difi-oppsett
+@Slf4j
 @Configuration
 public class DifiEsigneringConfig {
+
+  @Autowired
+  private SecretManagerTemplate secretManagerTemplate;
 
   @Value("${farskapsportal-api.disable-esignering}")
   private boolean disableEsignering;
 
   @Bean
-  public KeyStoreConfig keyStoreConfig(@Value("${sm://projects/virksomhetssertifikat-dev/secrets/test-virksomhetssertifikat-felles_2018-2021}") String sertifikatP12,
+  public KeyStoreConfig keyStoreConfig(
+      @Value("${sm://projects/virksomhetssertifikat-dev/secrets/test-virksomhetssertifikat-felles_2018-2021}") String sertifikatP12,
       @Value("{sm://virksomhetssertifikat-test-passord}") String sertifikatP12Passord) throws IOException {
+
+    log.info("Oppretter secret..");
+    createSecret("test-cert", sertifikatP12, "farskapsportal-dev-169c");
+
     return disableEsignering ? testKeyStoreConfig()
         : KeyStoreConfig.fromOrganizationCertificate(IOUtils.toInputStream(sertifikatP12, Charset.defaultCharset()), sertifikatP12Passord);
   }
@@ -58,5 +69,15 @@ public class DifiEsigneringConfig {
   @Bean
   public DifiESignaturConsumer difiESignaturConsumer(ClientConfiguration clientConfiguration, ModelMapper modelMapper, DirectClient directClient) {
     return new DifiESignaturConsumer(clientConfiguration, modelMapper, directClient, disableEsignering);
+  }
+
+  private void createSecret(String secretId, String secretPayload, String projectId) {
+    log.info("lengde sertifikat: {}", secretPayload.length());
+    if (StringUtils.isEmpty(projectId)) {
+      this.secretManagerTemplate.createSecret(secretId, secretPayload);
+    } else {
+      this.secretManagerTemplate.createSecret(secretId, secretPayload.getBytes(), projectId);
+    }
+
   }
 }
