1c987dcb5344b6aa34e1a039d7015ff6de7c919e
s148719 <jan.kjetil.skar@nav.no>
2021-02-09 17:11:21 +0100

    add sert to secret

--- src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
+++ src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
@@ -2,7 +2,12 @@ package no.nav.farskapsportal.config;
 
 import static no.nav.farskapsportal.FarskapsportalApplication.PROFILE_LIVE;
 
+import com.google.cloud.secretmanager.v1.Secret;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1.SecretName;
 import com.google.cloud.spring.secretmanager.SecretManagerTemplate;
+import com.google.protobuf.FieldMask;
+import com.google.protobuf.util.FieldMaskUtil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -13,6 +18,7 @@ import no.digipost.signature.client.ServiceUri;
 import no.digipost.signature.client.direct.DirectClient;
 import no.digipost.signature.client.security.KeyStoreConfig;
 import no.nav.farskapsportal.consumer.esignering.DifiESignaturConsumer;
+import no.nav.farskapsportal.gcp.secretmanager.AddSecretVersion;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
@@ -26,22 +32,24 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class DifiEsigneringConfig {
 
-  @Autowired(required = false)
-  private SecretManagerTemplate secretManagerTemplate;
-
   @Value("${farskapsportal-api.disable-esignering}")
   private boolean disableEsignering;
 
   @Bean
   public KeyStoreConfig keyStoreConfig(
       @Value("${sm://projects/virksomhetssertifikat-dev/secrets/test-virksomhetssertifikat-felles_2018-2021}") String sertifikatP12,
-      @Value("${sm://projects/627047445397/secrets/virksomhetssertifikat-test-passord/versions/1}") String sertifikatP12Passord) throws IOException {
+      @Value("${sm://projects/627047445397/secrets/virksomhetssertifikat-test-passord/versions/1}") String sertifikatP12Passord,
+      @Autowired(required = false) SecretManagerTemplate secretManagerTemplate, @Autowired(required = false) AddSecretVersion addSecretVersion)
+      throws IOException {
 
     log.info("sert-pwd lengde: {}", sertifikatP12Passord.length());
 
     if (!disableEsignering) {
       log.info("Oppretter secret..");
-      createSecret("test-cert", sertifikatP12, "farskapsportal-dev-169c");
+
+      var sercretId = "test-cert";
+      var projectId = "farskapsportal-dev-169c";
+      addSecretVersion.addSecretVersion(sercretId, projectId, sertifikatP12);
     }
 
     return disableEsignering ? testKeyStoreConfig()
@@ -76,14 +84,12 @@ public class DifiEsigneringConfig {
   }
 
 
-
-  private void createSecret(String secretId, String secretPayload, String projectId) {
+  private void createSecret(SecretManagerTemplate secretManagerTemplate, String secretId, String secretPayload, String projectId) {
     log.info("lengde sertifikat: {}", secretPayload.length());
     if (StringUtils.isEmpty(projectId)) {
-      this.secretManagerTemplate.createSecret(secretId, secretPayload);
+      secretManagerTemplate.createSecret(secretId, secretPayload);
     } else {
-      this.secretManagerTemplate.createSecret(secretId, secretPayload.getBytes(), projectId);
+      secretManagerTemplate.createSecret(secretId, secretPayload.getBytes(), projectId);
     }
-
   }
-  }
+}
