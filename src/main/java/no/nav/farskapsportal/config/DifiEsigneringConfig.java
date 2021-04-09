6ee17b19695a0380c619ee8a860879474025d0b5
s148719 <jan.kjetil.skar@nav.no>
2021-04-09 13:51:04 +0200

    Legge til Live-konfig for SkattConsumer

--- src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
+++ src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
@@ -4,6 +4,10 @@ import static no.nav.farskapsportal.FarskapsportalApplication.PROFILE_LIVE;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import lombok.extern.slf4j.Slf4j;
 import no.digipost.signature.client.Certificates;
 import no.digipost.signature.client.ClientConfiguration;
--- src/main/java/no/nav/farskapsportal/config/RestTemplateConfig.java
+++ src/main/java/no/nav/farskapsportal/config/RestTemplateConfig.java
@@ -3,17 +3,28 @@ package no.nav.farskapsportal.config;
 import static no.nav.farskapsportal.config.FarskapsportalConfig.X_API_KEY;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import lombok.extern.slf4j.Slf4j;
+import no.digipost.signature.client.security.KeyStoreConfig;
 import no.nav.bidrag.commons.web.CorrelationIdFilter;
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate;
 import no.nav.farskapsportal.consumer.sts.SecurityTokenServiceConsumer;
 import org.apache.commons.lang3.Validate;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
 @Slf4j
@@ -23,6 +34,18 @@ public class RestTemplateConfig {
   private static final String TEMA = "Tema";
   private static final String TEMA_FAR = "FAR";
 
+  @Value("${consumer.skatt.maks-antall-forbindelser}")
+  private int consumerSkattMaksAntallForbindelser;
+
+  @Value("${consumer.skatt.maks-antall-forbindelser-per-rute}")
+  private int consumerSkattMaksAntallForbindelserPerRute;
+
+  @Value("${consumer.skatt.maks-ventetid-forbindelse}")
+  private int consumerSkattMaksVentetidForbindelse;
+
+  @Value("${consumer.skatt.maks-ventetid-lesing}")
+  private int consumerSkattMaksVentetidLesing;
+  
   @Bean
   @Qualifier("base")
   @Scope("prototype")
@@ -39,14 +62,6 @@ public class RestTemplateConfig {
       @Value("${APIKEY_STS_FP}") String xApiKeySts) {
     log.info("Setter {} for STS", X_API_KEY);
     httpHeaderRestTemplate.addHeaderGenerator(X_API_KEY, () -> xApiKeySts);
-    return httpHeaderRestTemplate;
-  }
-
-  @Bean
-  @Qualifier("skatt")
-  @Scope("prototype")
-  public HttpHeaderRestTemplate skattRestTemplate(@Qualifier("base") HttpHeaderRestTemplate httpHeaderRestTemplate) {
-      // TODO: fyll inn med virksomhetssertifikat fra GCP secret
     return httpHeaderRestTemplate;
   }
 
@@ -71,6 +86,30 @@ public class RestTemplateConfig {
     Validate.isTrue(!xApiKeyPdlApi.isBlank());
 
     httpHeaderRestTemplate.addHeaderGenerator(X_API_KEY, () -> xApiKeyPdlApi);
+    return httpHeaderRestTemplate;
+  }
+
+  @Bean
+  @Qualifier("skatt")
+  public HttpHeaderRestTemplate skattRestTemplate(@Qualifier("base") HttpHeaderRestTemplate httpHeaderRestTemplate, KeyStoreConfig keyStoreConfig)
+      throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException, KeyManagementException {
+
+    var socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder()
+        .loadTrustMaterial(null, new TrustAllStrategy())
+        .loadKeyMaterial(keyStoreConfig.keyStore, keyStoreConfig.keystorePassword.toCharArray()).build(),
+        NoopHostnameVerifier.INSTANCE);
+
+    var httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory)
+        .setMaxConnTotal(consumerSkattMaksAntallForbindelser)
+        .setMaxConnPerRoute(consumerSkattMaksAntallForbindelserPerRute)
+        .build();
+
+    var requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    requestFactory.setReadTimeout(consumerSkattMaksVentetidLesing);
+    requestFactory.setConnectTimeout(consumerSkattMaksVentetidForbindelse);
+
+    httpHeaderRestTemplate.setRequestFactory(requestFactory);
+
     return httpHeaderRestTemplate;
   }
 }
