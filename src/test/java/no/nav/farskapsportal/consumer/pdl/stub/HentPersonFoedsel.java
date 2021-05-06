d6ffbf3ba178bfe33b424e2cfe45aed4b2693b5b
s148719 <jan.kjetil.skar@nav.no>
2021-05-06 08:30:13 +0200

    merge

--- src/test/java/no/nav/farskapsportal/consumer/pdl/stub/HentPersonFoedsel.java
+++ src/test/java/no/nav/farskapsportal/consumer/pdl/stub/HentPersonFoedsel.java
@@ -5,37 +5,26 @@ import static no.nav.farskapsportal.service.FarskapsportalService.KODE_LAND_NORG
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Value;
-import no.nav.farskapsportal.service.FarskapsportalService;
 
 @Value
 @Getter
 public class HentPersonFoedsel implements HentPersonSubResponse {
+
   String response;
 
   public HentPersonFoedsel(LocalDate foedselsdato, boolean historisk) {
-<<<<<<< HEAD
-    this.response = buildResponse(foedselsdato,  KODE_LAND_NORGE, "ASKIM", "123", historisk);
+    this.response = buildResponse(foedselsdato, KODE_LAND_NORGE, "ASKIM", "123", historisk);
   }
 
   public HentPersonFoedsel(LocalDate foedselsdato, String foedested, boolean historisk) {
     this.response = buildResponse(foedselsdato, KODE_LAND_NORGE, foedested, "123", historisk);
   }
 
-  public HentPersonFoedsel(LocalDate foedselsdato, String foedeland,  String foedested, boolean historisk) {
+  public HentPersonFoedsel(LocalDate foedselsdato, String foedeland, String foedested, boolean historisk) {
     this.response = buildResponse(foedselsdato, foedeland, foedested, "123", historisk);
   }
 
-  private String buildResponse(LocalDate foedselsdato, String foedeland,  String foedested, String opplysningsId, boolean historisk) {
-=======
-    this.response = buildResponse(foedselsdato, "ASKIM", "123", historisk);
-  }
-
-  public HentPersonFoedsel(LocalDate foedselsdato, String foedested, boolean historisk) {
-    this.response = buildResponse(foedselsdato, foedested, "123", historisk);
-  }
-
-  private String buildResponse(LocalDate foedselsdato, String foedested, String opplysningsId, boolean historisk) {
->>>>>>> main
+  private String buildResponse(LocalDate foedselsdato, String foedeland, String foedested, String opplysningsId, boolean historisk) {
     if (foedselsdato == null) {
       return String.join("\n", " \"foedsel\": [", "]");
     } else {
@@ -46,10 +35,7 @@ public class HentPersonFoedsel implements HentPersonSubResponse {
           " \"foedsel\": [",
           " {",
           " \"foedselsdato\": \"" + fd + "\",",
-<<<<<<< HEAD
           " \"foedeland\": \"" + foedeland + "\",",
-=======
->>>>>>> main
           " \"foedested\": \"" + foedested + "\",",
           " \"metadata\": {",
           " \"opplysningsId\": \"" + opplysningsId + "\",",
