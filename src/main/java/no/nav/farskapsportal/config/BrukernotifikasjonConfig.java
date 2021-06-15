e3c193f442f002fd05e82cfbd9ce094bce36754e
s148719 <jan.kjetil.skar@nav.no>
2021-06-15 08:42:22 +0200

    Update BrukernotifikasjonConfig.java

--- src/main/java/no/nav/farskapsportal/config/BrukernotifikasjonConfig.java
+++ src/main/java/no/nav/farskapsportal/config/BrukernotifikasjonConfig.java
@@ -72,7 +72,7 @@ public class BrukernotifikasjonConfig {
     configProps.put("security.protocol", securityProtocol);
     configProps.put("sasl.jaas.config", saslJaasConfig);
     configProps.put("sasl.mechanism", saslMechanism);
-    configProps.put("reconnect.backoff.ms", 500);
+    configProps.put("reconnect.backoff.ms", 100);
     return configProps;
   }
 
