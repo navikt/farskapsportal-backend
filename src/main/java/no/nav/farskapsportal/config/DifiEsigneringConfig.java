4e2a7d59d3ef5223a8040f37d2a2081939a66137
s148719 <jan.kjetil.skar@nav.no>
2021-06-20 11:15:12 +0200

    Parameterstyre miljø for esignering

--- src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
+++ src/main/java/no/nav/farskapsportal/config/DifiEsigneringConfig.java
@@ -12,6 +12,7 @@ import no.digipost.signature.client.security.KeyStoreConfig;
 import no.nav.farskapsportal.config.egenskaper.FarskapsportalEgenskaper;
 import no.nav.farskapsportal.consumer.esignering.DifiESignaturConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,15 +20,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DifiEsigneringConfig {
 
+  private String miljoe;
   private FarskapsportalEgenskaper farskapsportalEgenskaper;
 
-  public DifiEsigneringConfig(@Autowired FarskapsportalEgenskaper farskapsportalEgenskaper) {
+  public DifiEsigneringConfig(@Autowired FarskapsportalEgenskaper farskapsportalEgenskaper, @Value("${NAV_CLUSTER_NAME}") String navClusterName) {
     this.farskapsportalEgenskaper = farskapsportalEgenskaper;
+    this.miljoe = navClusterName.equals(NavClusterName.PROD) ? NavClusterName.PROD.toString() : NavClusterName.TEST.toString();
   }
 
   @Bean
   public ClientConfiguration clientConfiguration(KeyStoreConfig keyStoreConfig) {
-    return ClientConfiguration.builder(keyStoreConfig).trustStore(Certificates.TEST).serviceUri(ServiceUri.DIFI_TEST)
+
+    var certificates = miljoe.equals(NavClusterName.TEST) ? Certificates.TEST : Certificates.PRODUCTION;
+    var serviceUrl = miljoe.equals(NavClusterName.TEST) ? ServiceUri.DIFI_TEST : ServiceUri.PRODUCTION;
+
+    return ClientConfiguration.builder(keyStoreConfig).trustStore(certificates).serviceUri(serviceUrl)
         .globalSender(new Sender(farskapsportalEgenskaper.getNavOrgnummer())).build();
   }
 
@@ -45,5 +52,18 @@ public class DifiEsigneringConfig {
             URI.create(farskapsportalEgenskaper.getEsignering().getFeiletUrl()));
 
     return new DifiESignaturConsumer(exitUrls, directClient);
+  }
+
+  private enum NavClusterName {
+    TEST("dev-gcp"), PROD("prod-gcp");
+    String clusterName;
+
+    NavClusterName(String beskrivelse) {
+      this.clusterName = beskrivelse;
+    }
+
+    public String getClusterName() {
+      return this.clusterName;
+    }
   }
 }
