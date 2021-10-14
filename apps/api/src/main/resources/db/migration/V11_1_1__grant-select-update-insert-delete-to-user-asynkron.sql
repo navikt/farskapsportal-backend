9cf87005fec3c8cff25c0018af3d8be2af6dc3dc
s148719 <jan.kjetil.skar@nav.no>
2021-10-14 14:32:14 +0200

    Update V11_1_1__grant-select-update-insert-delete-to-user-asynkron.sql

--- apps/api/src/main/resources/db/migration/V11_1_1__grant-select-update-insert-delete-to-user-asynkron.sql
+++ apps/api/src/main/resources/db/migration/V11_1_1__grant-select-update-insert-delete-to-user-asynkron.sql
@@ -1,11 +1,11 @@
--- Lese og skrivetilgang til alle tabeller i public-skjema for user_asynkron
+-- Lese og skrivetilgang til alle tabeller i public-skjema for user
 
 /*
 REVOKE SELECT, INSERT, UPDATE, DELETE
     ON ALL TABLES IN SCHEMA public
-    FROM ${user_asynkron};
+    FROM ${user};
  */
 
 GRANT SELECT, INSERT, UPDATE, DELETE
     ON ALL TABLES IN SCHEMA public
-    TO ${user_asynkron};
+    TO ${user};
