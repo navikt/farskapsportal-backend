b2717e3e2bd3127791cbbab605e56c4f58c45206
Jan Kjetil Skår <43172034+s148719@users.noreply.github.com>
2021-12-30 12:40:11 +0100

    Legge på caching av STS-token (#349)
    * Legge på caching av STS-token

--- libs/felles/src/main/java/no/nav/farskapsportal/backend/libs/felles/config/CachingConfig.java
+++ libs/felles/src/main/java/no/nav/farskapsportal/backend/libs/felles/config/CachingConfig.java
@@ -7,21 +7,24 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableCaching
 public class CachingConfig {
 
   @Bean
-  public Caffeine caffeineConfig() {
-    return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
-  }
-
-  @Bean
-  public CacheManager cacheManager(Caffeine caffeine) {
+  @Primary
+  public CacheManager cacheManager() {
     CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-    caffeineCacheManager.setCaffeine(caffeine);
+    caffeineCacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES));
     return caffeineCacheManager;
   }
 
+  @Bean
+  public CacheManager cacheManagerSts() {
+    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+    caffeineCacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(45, TimeUnit.MINUTES));
+    return caffeineCacheManager;
+  }
 }
