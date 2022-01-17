54b52dc1edccbf799e0eb94e918ce0f1acc1799b
s148719 <jan.kjetil.skar@nav.no>
2022-01-17 14:28:03 +0100

    Update DeaktivereFarskapserklaeringer.java

--- apps/asynkron/src/main/java/no/nav/farskapsportal/backend/apps/asynkron/scheduled/DeaktivereFarskapserklaeringer.java
+++ apps/asynkron/src/main/java/no/nav/farskapsportal/backend/apps/asynkron/scheduled/DeaktivereFarskapserklaeringer.java
@@ -38,6 +38,9 @@ public class DeaktivereFarskapserklaeringer {
     if (idTilFarskapserklaeringerMedUtgaatteSigneringsoppdrag.size() > 0) {
       log.info("Fant {} ikke-ferdigstilte farskapserklæringer med signeringsoppdrag eldre enn 40 dager. Deaktiverer disse.",
           idTilFarskapserklaeringerMedUtgaatteSigneringsoppdrag.size());
+    } else {
+      log.info("Fant ingen ikke-ferdigstilte farskapserklæringer med signeringsoppdrag eldre enn 40 dager");
+      return;
     }
 
     for (int farskapserklaeringsid : idTilFarskapserklaeringerMedUtgaatteSigneringsoppdrag) {
@@ -77,17 +80,19 @@ public class DeaktivereFarskapserklaeringer {
 
     var idTilFarskapserklaeringerSomSkalDeaktiveres = persistenceService.henteIdTilOversendteFarskapserklaeringerSomErKlarForDeaktivering(
         tidspunktOversendtFoer);
+
     if (idTilFarskapserklaeringerSomSkalDeaktiveres.size() > 0) {
       log.info("Fant {} ferdigstilte farskapserklæringer som har blitt overført til skatt og er klare for deaktivering.",
           idTilFarskapserklaeringerSomSkalDeaktiveres.size());
+      for (int farskapserklaeringsid : idTilFarskapserklaeringerSomSkalDeaktiveres) {
+        antallErklaeringerSomBleDeaktivert =
+            persistenceService.deaktivereFarskapserklaering(farskapserklaeringsid) ? ++antallErklaeringerSomBleDeaktivert
+                : antallErklaeringerSomBleDeaktivert;
+      }
+      log.info("{} ferdigstilte farskapserklæringer ble i denne omgang deaktivert", antallErklaeringerSomBleDeaktivert);
+    } else {
+      log.info("Fant ingen ferdigstilte farskapserklæringer som har blitt overført til skatt og er klare for deaktivering");
     }
-    for (int farskapserklaeringsid : idTilFarskapserklaeringerSomSkalDeaktiveres) {
-      antallErklaeringerSomBleDeaktivert =
-          persistenceService.deaktivereFarskapserklaering(farskapserklaeringsid) ? ++antallErklaeringerSomBleDeaktivert
-              : antallErklaeringerSomBleDeaktivert;
-    }
-
-    log.info("{} ferdigstilte farskapserklæringer ble i denne omgang deaktivert", antallErklaeringerSomBleDeaktivert);
   }
 
   private void deaktivereFarskapserklaeringerSomManglerMorsSignatur() {
@@ -100,13 +105,15 @@ public class DeaktivereFarskapserklaeringer {
         morSendtTilSigneringFoer);
     if (idTilFarskapserklaeringerSomSkalDeaktiveres.size() > 0) {
       log.info("Fant {} farskapserklæringer som ikke er signert av mor - deaktiverer disse.", idTilFarskapserklaeringerSomSkalDeaktiveres.size());
-    }
-    for (int farskapserklaeringsid : idTilFarskapserklaeringerSomSkalDeaktiveres) {
-      antallErklaeringerSomBleDeaktivert =
-          persistenceService.deaktivereFarskapserklaering(farskapserklaeringsid) ? ++antallErklaeringerSomBleDeaktivert
-              : antallErklaeringerSomBleDeaktivert;
-    }
+      for (int farskapserklaeringsid : idTilFarskapserklaeringerSomSkalDeaktiveres) {
+        antallErklaeringerSomBleDeaktivert =
+            persistenceService.deaktivereFarskapserklaering(farskapserklaeringsid) ? ++antallErklaeringerSomBleDeaktivert
+                : antallErklaeringerSomBleDeaktivert;
+      }
 
-    log.info("{} usignerte farskapserklæringer ble i denne omgang deaktivert", antallErklaeringerSomBleDeaktivert);
+      log.info("{} usignerte farskapserklæringer ble i denne omgang deaktivert", antallErklaeringerSomBleDeaktivert);
+    } else {
+      log.info("Fant ingen farskapserklæringer som ikke er signert av mor.");
+    }
   }
 }
