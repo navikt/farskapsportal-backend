07c3bdbf93df9abfa2af9f6dd9d5d3a2a8af7c48
s148719 <jan.kjetil.skar@nav.no>
2022-10-04 14:48:55 +0200

    div

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/provider/rs/status/StatusController.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/provider/rs/status/StatusController.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
