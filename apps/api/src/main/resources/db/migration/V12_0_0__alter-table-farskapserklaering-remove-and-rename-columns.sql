6dcc725fbd862dd3a53d081ebd6b3a44056cbb7a
s148719 <jan.kjetil.skar@nav.no>
2022-10-05 12:50:50 +0200

    WIP

--- apps/api/src/main/resources/db/migration/V12_0_0__alter-table-farskapserklaering-remove-and-rename-columns.sql
+++ apps/api/src/main/resources/db/migration/V12_0_0__alter-table-farskapserklaering-remove-and-rename-columns.sql
@@ -5,6 +5,8 @@ ALTER TABLE public.farskapserklaering
     DROP COLUMN oppgave_sendt,
     ADD COLUMN joark_journalpost_id varchar(50),
     ADD COLUMN sendt_til_joark timestamp without time zone;
+
+delete from flyway_schema_history where version = '12.0.0';
  */
 
 ALTER TABLE public.farskapserklaering
