0aedfcb0dd110a0f8116a87109e401e63b5419d6
s148719 <jan.kjetil.skar@nav.no>
2022-10-05 14:43:24 +0200

    Update FarskapsportalApiApplication.java

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/FarskapsportalApiApplication.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/FarskapsportalApiApplication.java
@@ -5,12 +5,14 @@ import static no.nav.farskapsportal.backend.libs.felles.config.FarskapsportalFel
 import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @EnableJwtTokenValidation(ignore = {"org.springdoc", "org.springframework"})
 @ConfigurationPropertiesScan("no.nav.farskapsportal.backend.apps.api.config.egenskaper")
 @ComponentScan({"no.nav.farskapsportal.backend.apps.api", "no.nav.farskapsportal.backend.libs"})
