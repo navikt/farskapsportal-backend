0b16878df17e4709363f5fd00d0c8ea86a849b40
s148719 <jan.kjetil.skar@nav.no>
2023-06-23 15:40:10 +0200

    statuspoll, fjerne asynkron db-bruker

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/config/FarskapsportalApiConfig.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/config/FarskapsportalApiConfig.java
@@ -285,14 +285,10 @@ public class FarskapsportalApiConfig {
 
     @Autowired
     public FlywayConfiguration(
-        @Qualifier("dataSource") DataSource dataSource,
-        @Value("${spring.flyway.placeholders.user}") String dbUserAsynkron)
+        @Qualifier("dataSource") DataSource dataSource)
         throws InterruptedException {
       Thread.sleep(30000);
-      var placeholders = new HashMap<String, String>();
-      placeholders.put("user_asynkron", dbUserAsynkron);
-
-      Flyway.configure().dataSource(dataSource).placeholders(placeholders).load().migrate();
+      Flyway.configure().dataSource(dataSource).load().migrate();
     }
   }
 
