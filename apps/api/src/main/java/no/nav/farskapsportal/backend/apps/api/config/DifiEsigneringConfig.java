ed0026179c926bd5e507400bb86280151101cdeb
s148719 <jan.kjetil.skar@nav.no>
2023-06-28 14:35:46 +0200

    Endre esignering-timeout fra default 10 til 15 sek

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/config/DifiEsigneringConfig.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/config/DifiEsigneringConfig.java
@@ -1,6 +1,13 @@
 package no.nav.farskapsportal.backend.apps.api.config;
 
+import static java.time.Duration.ofMillis;
+import static no.nav.farskapsportal.backend.libs.felles.config.FarskapsportalFellesConfig.PROFILE_INTEGRATION_TEST;
+import static no.nav.farskapsportal.backend.libs.felles.config.FarskapsportalFellesConfig.PROFILE_LIVE;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
 import no.digipost.signature.client.ClientConfiguration;
 import no.digipost.signature.client.ServiceEnvironment;
@@ -12,94 +19,118 @@ import no.nav.farskapsportal.backend.apps.api.consumer.esignering.DifiESignaturC
 import no.nav.farskapsportal.backend.libs.felles.secretmanager.AccessSecretVersion;
 import no.nav.farskapsportal.backend.libs.felles.secretmanager.FarskapKeystoreCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.Locale;
-
-import static no.nav.farskapsportal.backend.libs.felles.config.FarskapsportalFellesConfig.PROFILE_INTEGRATION_TEST;
-import static no.nav.farskapsportal.backend.libs.felles.config.FarskapsportalFellesConfig.PROFILE_LIVE;
-
 @Slf4j
 @Configuration
 @ComponentScan("no.nav.farskapsportal.backend.libs.felles")
 public class DifiEsigneringConfig {
 
-    private final FarskapsportalApiEgenskaper farskapsportalApiEgenskaper;
-    private final String miljoe;
+  private final FarskapsportalApiEgenskaper farskapsportalApiEgenskaper;
+  private final String miljoe;
 
-    public DifiEsigneringConfig(@Autowired FarskapsportalApiEgenskaper farskapsportalApiEgenskaper,
-                                @Value("${NAIS_CLUSTER_NAME}") String navClusterName) {
-        this.farskapsportalApiEgenskaper = farskapsportalApiEgenskaper;
-        this.miljoe = navClusterName.equals(NavClusterName.PROD.getClusterName()) ? NavClusterName.PROD.toString() : NavClusterName.TEST.toString();
+  public DifiEsigneringConfig(
+      @Autowired FarskapsportalApiEgenskaper farskapsportalApiEgenskaper,
+      @Value("${NAIS_CLUSTER_NAME}") String navClusterName) {
+    this.farskapsportalApiEgenskaper = farskapsportalApiEgenskaper;
+    this.miljoe =
+        navClusterName.equals(NavClusterName.PROD.getClusterName())
+            ? NavClusterName.PROD.toString()
+            : NavClusterName.TEST.toString();
+  }
+
+  @Bean
+  @Profile({PROFILE_LIVE, PROFILE_INTEGRATION_TEST})
+  public KeyStoreConfig keyStoreConfig(
+      @Value("${virksomhetssertifikat.prosjektid}") String virksomhetssertifikatProsjektid,
+      @Value("${virksomhetssertifikat.hemmelighetnavn}")
+          String virksomhetssertifikatHemmelighetNavn,
+      @Value("${virksomhetssertifikat.hemmelighetversjon}")
+          String virksomhetssertifikatHemmelighetVersjon,
+      @Value("${virksomhetssertifikat.passord.prosjektid}")
+          String virksomhetssertifikatPassordProsjektid,
+      @Value("${virksomhetssertifikat.passord.hemmelighetnavn}")
+          String virksomhetssertifikatPassordHemmelighetNavn,
+      @Value("${virksomhetssertifikat.passord.hemmelighetversjon}")
+          String virksomhetssertifikatPassordHemmelighetVersjon,
+      @Autowired(required = false) AccessSecretVersion accessSecretVersion)
+      throws IOException {
+
+    var sertifikatpassord =
+        accessSecretVersion
+            .accessSecretVersion(
+                virksomhetssertifikatPassordProsjektid,
+                virksomhetssertifikatPassordHemmelighetNavn,
+                virksomhetssertifikatPassordHemmelighetVersjon)
+            .getData()
+            .toStringUtf8();
+
+    var objectMapper = new ObjectMapper();
+    var farskapKeystoreCredentials =
+        objectMapper.readValue(sertifikatpassord, FarskapKeystoreCredentials.class);
+
+    log.info("lengde sertifikatpassord {}", farskapKeystoreCredentials.getPassword().length());
+
+    var secretPayload =
+        accessSecretVersion.accessSecretVersion(
+            virksomhetssertifikatProsjektid,
+            virksomhetssertifikatHemmelighetNavn,
+            virksomhetssertifikatHemmelighetVersjon);
+
+    log.info("lengde sertifikat: {}", secretPayload.getData().size());
+    var inputStream = new ByteArrayInputStream(secretPayload.getData().toByteArray());
+
+    return KeyStoreConfig.fromJavaKeyStore(
+        inputStream,
+        farskapKeystoreCredentials.getAlias(),
+        farskapKeystoreCredentials.getPassword(),
+        farskapKeystoreCredentials.getPassword());
+  }
+
+  @Bean
+  @Profile({PROFILE_LIVE, PROFILE_INTEGRATION_TEST})
+  public ClientConfiguration clientConfiguration(KeyStoreConfig keyStoreConfig) {
+    var serviceEnvironment =
+        miljoe.equals(NavClusterName.TEST.toString())
+            ? ServiceEnvironment.STAGING
+            : ServiceEnvironment.PRODUCTION;
+    log.info(
+        "Kobler opp mot Postens {}-milljø for esignering med service-uri {}.",
+        miljoe.toLowerCase(Locale.ROOT),
+        serviceEnvironment.signatureServiceRootUrl());
+    return ClientConfiguration.builder(keyStoreConfig)
+        .timeouts(t -> t.responseArrivalTimeout(ofMillis(15000)))
+        .serviceEnvironment(serviceEnvironment)
+        .defaultSender(new Sender(farskapsportalApiEgenskaper.getNavOrgnummer()))
+        .build();
+  }
+
+  @Bean
+  public DirectClient directClient(ClientConfiguration clientConfiguration) {
+    return new DirectClient(clientConfiguration);
+  }
+
+  @Bean
+  public DifiESignaturConsumer difiESignaturConsumer(DirectClient directClient) {
+
+    return new DifiESignaturConsumer(farskapsportalApiEgenskaper, directClient);
+  }
+
+  private enum NavClusterName {
+    TEST("dev-gcp"),
+    PROD("prod-gcp");
+    String clusterName;
+
+    NavClusterName(String clusterName) {
+      this.clusterName = clusterName;
     }
 
-    @Bean
-    @Profile({PROFILE_LIVE, PROFILE_INTEGRATION_TEST})
-    public KeyStoreConfig keyStoreConfig(
-            @Value("${virksomhetssertifikat.prosjektid}") String virksomhetssertifikatProsjektid,
-            @Value("${virksomhetssertifikat.hemmelighetnavn}") String virksomhetssertifikatHemmelighetNavn,
-            @Value("${virksomhetssertifikat.hemmelighetversjon}") String virksomhetssertifikatHemmelighetVersjon,
-            @Value("${virksomhetssertifikat.passord.prosjektid}") String virksomhetssertifikatPassordProsjektid,
-            @Value("${virksomhetssertifikat.passord.hemmelighetnavn}") String virksomhetssertifikatPassordHemmelighetNavn,
-            @Value("${virksomhetssertifikat.passord.hemmelighetversjon}") String virksomhetssertifikatPassordHemmelighetVersjon,
-            @Autowired(required = false) AccessSecretVersion accessSecretVersion) throws IOException {
-
-        var sertifikatpassord = accessSecretVersion
-                .accessSecretVersion(virksomhetssertifikatPassordProsjektid, virksomhetssertifikatPassordHemmelighetNavn,
-                        virksomhetssertifikatPassordHemmelighetVersjon).getData().toStringUtf8();
-
-        var objectMapper = new ObjectMapper();
-        var farskapKeystoreCredentials = objectMapper.readValue(sertifikatpassord, FarskapKeystoreCredentials.class);
-
-        log.info("lengde sertifikatpassord {}", farskapKeystoreCredentials.getPassword().length());
-
-        var secretPayload = accessSecretVersion
-                .accessSecretVersion(virksomhetssertifikatProsjektid, virksomhetssertifikatHemmelighetNavn, virksomhetssertifikatHemmelighetVersjon);
-
-        log.info("lengde sertifikat: {}", secretPayload.getData().size());
-        var inputStream = new ByteArrayInputStream(secretPayload.getData().toByteArray());
-
-        return KeyStoreConfig
-                .fromJavaKeyStore(inputStream, farskapKeystoreCredentials.getAlias(), farskapKeystoreCredentials.getPassword(),
-                        farskapKeystoreCredentials.getPassword());
+    public String getClusterName() {
+      return this.clusterName;
     }
-
-    @Bean
-    @Profile({PROFILE_LIVE, PROFILE_INTEGRATION_TEST})
-    public ClientConfiguration clientConfiguration(KeyStoreConfig keyStoreConfig) {
-        var serviceEnvironment = miljoe.equals(NavClusterName.TEST.toString()) ? ServiceEnvironment.STAGING : ServiceEnvironment.PRODUCTION;
-        log.info("Kobler opp mot Postens {}-milljø for esignering med service-uri {}.", miljoe.toLowerCase(Locale.ROOT), serviceEnvironment.signatureServiceRootUrl());
-        return ClientConfiguration.builder(keyStoreConfig).serviceEnvironment(serviceEnvironment).defaultSender(new Sender(farskapsportalApiEgenskaper.getNavOrgnummer())).build();
-    }
-
-    @Bean
-    public DirectClient directClient(ClientConfiguration clientConfiguration) {
-        return new DirectClient(clientConfiguration);
-    }
-
-    @Bean
-    public DifiESignaturConsumer difiESignaturConsumer(DirectClient directClient) {
-
-        return new DifiESignaturConsumer(farskapsportalApiEgenskaper, directClient);
-    }
-
-    private enum NavClusterName {
-        TEST("dev-gcp"), PROD("prod-gcp");
-        String clusterName;
-
-        NavClusterName(String clusterName) {
-            this.clusterName = clusterName;
-        }
-
-        public String getClusterName() {
-            return this.clusterName;
-        }
-    }
+  }
 }
