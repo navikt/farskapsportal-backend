1685b8081553bb983cd30a8ee45cd96d0607ac60
s148719 <jan.kjetil.skar@nav.no>
2023-10-03 15:31:59 +0200

    Update FarskapsportalService.java

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/service/FarskapsportalService.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/service/FarskapsportalService.java
@@ -480,7 +480,8 @@ public class FarskapsportalService {
 
       farskapserklaering.getDokument().setBlobIdGcp(blobIdGcpPades);
       farskapserklaering.getDokument().getDokumentinnhold().setInnhold(null);
-      log.info("PAdES for farskapserklæring med id {} ble migrert til bøtte.", idFarskapserklaering);
+      log.info(
+          "PAdES for farskapserklæring med id {} ble migrert til bøtte.", idFarskapserklaering);
     }
 
     if (farskapserklaering.getDokument().getSigneringsinformasjonMor().getXadesXml() != null) {
@@ -493,7 +494,9 @@ public class FarskapsportalService {
           .getSigneringsinformasjonMor()
           .setBlobIdGcp(blobIdGcpXadesMor);
       farskapserklaering.getDokument().getSigneringsinformasjonMor().setXadesXml(null);
-      log.info("Mors XAdES for farskapserklæring med id {} ble migrert til bøtte.", idFarskapserklaering);
+      log.info(
+          "Mors XAdES for farskapserklæring med id {} ble migrert til bøtte.",
+          idFarskapserklaering);
     }
 
     if (farskapserklaering.getDokument().getSigneringsinformasjonFar().getXadesXml() != null) {
@@ -506,7 +509,9 @@ public class FarskapsportalService {
           .getSigneringsinformasjonMor()
           .setBlobIdGcp(blobIdGcpXadesFar);
       farskapserklaering.getDokument().getSigneringsinformasjonFar().setXadesXml(null);
-      log.info("Fars XAdES for farskapserklæring med id {} ble migrert til bøtte.", idFarskapserklaering);
+      log.info(
+          "Fars XAdES for farskapserklæring med id {} ble migrert til bøtte.",
+          idFarskapserklaering);
     }
   }
 
