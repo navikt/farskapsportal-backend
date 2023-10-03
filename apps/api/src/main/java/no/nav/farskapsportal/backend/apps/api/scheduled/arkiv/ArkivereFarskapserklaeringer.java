ead4ed9af27325d12ad9411ff9d1e4ece84e78c0
s148719 <jan.kjetil.skar@nav.no>
2023-10-03 12:31:51 +0200

    Spotify + avhengigheter

--- apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/scheduled/arkiv/ArkivereFarskapserklaeringer.java
+++ apps/api/src/main/java/no/nav/farskapsportal/backend/apps/api/scheduled/arkiv/ArkivereFarskapserklaeringer.java
@@ -46,7 +46,7 @@ public class ArkivereFarskapserklaeringer {
           LocalDateTime.now().plusSeconds(intervallMellomForsoek / 1000));
     }
   }
-  
+
   @SchedulerLock(name = "migrere-dokumenter", lockAtLeastFor = "PT1M", lockAtMostFor = "PT100M")
   @Scheduled(
       cron = "${farskapsportal.asynkron.egenskaper.arkiv.dokumentmigreringsrate}",
